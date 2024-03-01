48c43e29d61e4a3f7cb13d6f2e12e57b0186c53b
Rajat Gupta <rajatgupta0907@gmail.com>
2024-03-01 14:34:07 -0800

    resolved type

--- components/forms/PatientOrDoctor.tsx
+++ components/forms/PatientOrDoctor.tsx
@@ -4,16 +4,17 @@ import { useState } from "react";
 import { saveSpecialtyToUser } from "@/lib/actions/user.actions";
 import  {saveSpecialtyToDoctor} from '@/lib/actions/doctor.actions';
 interface Props{
-    userId : string
+    userId : string,
+    username: string
 }
-async function saveSpecialtyType(profileType: string, userId: string,setSuccessMessage: React.Dispatch<React.SetStateAction<boolean>>) {
+async function saveSpecialtyType(profileType: string, username: string,userId: string,setSuccessMessage: React.Dispatch<React.SetStateAction<boolean>>) {
 
     try {
         if (profileType === "patient") {
-            await saveSpecialtyToUser({ userId: userId, profileType: profileType });
+            await saveSpecialtyToUser({ userId: userId,username: username ,profileType: profileType });
             window.location.href= '/patient-dashboard';
         }else{
-            await saveSpecialtyToDoctor({userId: userId,profileType: profileType});
+            await saveSpecialtyToDoctor({userId: userId,username: username,profileType: profileType});
             window.location.href= '/doctor-admin';
 
         }
@@ -27,7 +28,7 @@ async function saveSpecialtyType(profileType: string, userId: string,setSuccessM
     }
 }
 
-const PatientOrDoctor = ({userId}:Props) =>{
+const PatientOrDoctor = ({userId, username}:Props) =>{
     const [showSuccessMessage, setShowSuccessMessage] = useState(false);
     const [selectedSpecialty, setSelectedSpecialty] = useState<string | null>(null);
 
@@ -40,7 +41,7 @@ const PatientOrDoctor = ({userId}:Props) =>{
                 <button onClick={() => { setSelectedSpecialty("doctor"); }}>Doctor</button>
             </div>
             {selectedSpecialty && (
-                <button onClick={() => saveSpecialtyType(selectedSpecialty, userId,setShowSuccessMessage)}>Save</button>
+                <button onClick={() => saveSpecialtyType(selectedSpecialty, username,userId,setShowSuccessMessage)}>Save</button>
             )}
             {showSuccessMessage && (
                 <p>Specialty type saved successfully!</p>
