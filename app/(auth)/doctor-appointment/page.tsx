c5a532c8d617e15b807d44e1e64fc62f36639bf1
Sugam Karki <sugamkarki7058@gmail.com>
2024-03-18 19:28:54 -0400

    added a button for chat

--- app/(auth)/doctor-appointment/page.tsx
+++ app/(auth)/doctor-appointment/page.tsx
@@ -1,88 +1,78 @@
-
 import { currentUser } from "@clerk/nextjs";
-import {redirect} from 'next/navigation';
+import { redirect } from "next/navigation";
 import DoctorNavbar from "@/components/navbars/DoctorNavbar";
-import {getAppointmentbyDoctor} from '@/lib/actions/appointment.actions'
+import { getAppointmentbyDoctor } from "@/lib/actions/appointment.actions";
 import DoctorAppointMentShow from "@/components/shared/DoctorAppointMentShow";
 import { fetchUser } from "@/lib/actions/user.actions";
 
 import appointment from "@/lib/models/appointment.model";
 import { fetchDoctor } from "@/lib/actions/admin.actions";
 const Page = async ({ searchParams }: any) => {
-
-    
   const user = await currentUser();
-  if(!user ){
-    redirect('/account');
+  if (!user) {
+    redirect("/account");
   }
-  const myAppointments = await getAppointmentbyDoctor({doctor_id: user.id});
- const doctorName =  await fetchDoctor(user.id);
- let name = "";
- if(!doctorName.name || doctorName.name == "" ){
+  const myAppointments = await getAppointmentbyDoctor({ doctor_id: user.id });
+  const doctorName = await fetchDoctor(user.id);
+  let name = "";
+  if (!doctorName.name || doctorName.name == "") {
     name = doctorName.username;
-    }else{
-        name = doctorName.name;
-    }
-    type AppointmentType = {
-        id: string;         patient_name: string;
-        doctor_name: string;
-        doctor_id: string;
-        patient_id: string;
-        appointment_time: string;
-        appointment_date: Date;
-      };
-      
+  } else {
+    name = doctorName.name;
+  }
+  type AppointmentType = {
+    id: string;
+    patient_name: string;
+    doctor_name: string;
+    doctor_id: string;
+    patient_id: string;
+    appointment_time: string;
+    appointment_date: Date;
+  };
+
   const newAppointment: AppointmentType[] = [];
- const appointmentsWithPatientNames = await Promise.all(myAppointments.map(async appointment => {
-    const patientName = await fetchUser(appointment.patient_id);
-    let patientString = "";
-    if(patientName.name == "" && !patientName.name){
-      patientString = patientName.username;
-    }else{
-      patientString = patientName.name;
-    }
+  const appointmentsWithPatientNames = await Promise.all(
+    myAppointments.map(async (appointment) => {
+      const patientName = await fetchUser(appointment.patient_id);
+      let patientString = "";
+      if (patientName.name == "" && !patientName.name) {
+        patientString = patientName.username;
+      } else {
+        patientString = patientName.name;
+      }
 
-
-   const localAppointment = {
-        id : appointment.id,
+      const localAppointment = {
+        id: appointment.id,
         patient_name: patientString,
-        doctor_name : name,
+        doctor_name: name,
         doctor_id: appointment.doctor_id,
-        patient_id : appointment.patient_id,
+        patient_id: appointment.patient_id,
         appointment_time: appointment.appointment_time,
-        appointment_date: appointment.appointment_date
-    }
+        appointment_date: appointment.appointment_date,
+      };
 
-    newAppointment.push(localAppointment);
+      newAppointment.push(localAppointment);
+    })
+  );
 
-
-}));
-
-
-console.log(newAppointment);
-
+  console.log(newAppointment);
 
   return (
-      <div className="bg-white">
-
+    <div className="bg-white">
       <DoctorNavbar />
       <div className="mt-[50px] pt-[50px]"></div>
-      {newAppointment.map(appointment => (
-        
-      <DoctorAppointMentShow 
-      id={appointment.id}
-      doctor_id = {appointment.doctor_id}
-      patient_id = {appointment.patient_id}
-      appointment_date = {appointment.appointment_date}
-      appointment_time = {appointment.appointment_time}
-      patient_name= {appointment.patient_name}
-      doctor_name = {appointment.doctor_name}
-      />
+      {newAppointment.map((appointment) => (
+        <DoctorAppointMentShow
+          id={appointment.id}
+          key={appointment.id}
+          doctor_id={appointment.doctor_id}
+          patient_id={appointment.patient_id}
+          appointment_date={appointment.appointment_date}
+          appointment_time={appointment.appointment_time}
+          patient_name={appointment.patient_name}
+          doctor_name={appointment.doctor_name}
+        />
       ))}
-
-       
-    
-     
     </div>
   );
 };
